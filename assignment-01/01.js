e475e410e313fc247bf324b37d2f88370fff1c33
Abdul Hamid <hamidkhattak469@gmail.com>
2023-11-09 00:29:46 +0500

    added more question or solved more question

--- assignment-01/01.js
+++ assignment-01/01.js
@@ -2,9 +2,21 @@ var myName = 'Hamid';
 console.log("Hello " + myName);
 var quote = "Learn and apply";
 console.log(myName + ": \t" + quote);
-var numOne = 5;
-var numTwo = 3;
-console.log(numOne + numTwo);
-console.log(numOne - numTwo);
-console.log(numOne * numTwo);
-console.log(numOne / numTwo);
+console.log(5 + 3);
+console.log(5 - 3);
+console.log(5 * 3);
+console.log(5 / 3);
+var favoriteNumber = 7;
+console.log("This is my favorite number: " + favoriteNumber);
+// Sigle line comment
+/**
+ * Multi line comment
+ * Hello this muli line comment
+ * Web 3.0 is start
+ */
+var names = ["Saqib", "Hamid", "Sahil", "Amir", "Shahbaz"];
+console.log(names);
+console.log("Hello " + names[0]);
+console.log("Hi " + names[1]);
+console.log("Hy" + names[3]);
+console.log("Bye " + names[4]);
--- assignment-01/01.ts
+++ assignment-01/01.ts
@@ -6,12 +6,27 @@ let quote: string = "Learn and apply";
 
 console.log(myName + ": \t" + quote);
 
+console.log(5 + 3);
+console.log(5 - 3);
+console.log(5 * 3);
+console.log(5 / 3);
 
-let numOne: number = 5;
-let numTwo: number = 3;
+
+let favoriteNumber: number = 7;
+console.log("This is my favorite number: " + favoriteNumber);
 
 
-console.log(numOne + numTwo);
-console.log(numOne - numTwo);
-console.log(numOne * numTwo);
-console.log(numOne / numTwo);
+// Sigle line comment
+/**
+ * Multi line comment
+ * Hello this muli line comment
+ * Web 3.0 is start
+ */
+
+const names: string[] = ["Saqib", "Hamid", "Sahil", "Amir", "Shahbaz"];
+console.log(names);
+
+console.log("Hello " + names[0]);
+console.log("Hi " + names[1]);
+console.log("Hy" + names[3]);
+console.log("Bye " + names[4]);
