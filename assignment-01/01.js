7bf43a47255461fbea5469405a836507747a63a3
Abdul Hamid <hamidkhattak469@gmail.com>
2023-11-12 03:22:22 +0500

    Question 14 complated

--- assignment-01/01.js
+++ assignment-01/01.js
@@ -1,22 +1,102 @@
-var myName = 'Hamid';
-console.log("Hello " + myName);
-var quote = "Learn and apply";
-console.log(myName + ": \t" + quote);
-console.log(5 + 3);
-console.log(5 - 3);
-console.log(5 * 3);
-console.log(5 / 3);
-var favoriteNumber = 7;
-console.log("This is my favorite number: " + favoriteNumber);
-// Sigle line comment
 /**
- * Multi line comment
- * Hello this muli line comment
- * Web 3.0 is start
+ * Question 1
  */
-var names = ["Saqib", "Hamid", "Sahil", "Amir", "Shahbaz"];
-console.log(names);
-console.log("Hello " + names[0]);
-console.log("Hi " + names[1]);
-console.log("Hy" + names[3]);
-console.log("Bye " + names[4]);
+/**
+ * Nodejs Typscript and VS Code installed
+ */
+/**
+ * Question 2
+ */
+var fullName = "Abdul Hamid";
+console.log("Hello this is ".concat(fullName));
+/**
+ * Question 3
+ */
+var studentName = "Sahil Khan";
+// Uppercase
+console.log(studentName.toUpperCase());
+// Lowercase
+console.log(studentName.toLowerCase());
+// Titlecase
+console.log("".concat(studentName.charAt(0).toUpperCase(), " ").concat(studentName.slice(1)));
+/**
+ * Question 4
+ */
+// Famous Quote by Albert Einstein
+var quote = "\"Success is Not Final, Failure is Not Fatal: it is the Courage to Continue that Counts\"";
+var authorName = "Winston Churchill";
+console.log("".concat(authorName, " said, ").concat(quote));
+/**
+ * Question 5
+ */
+var famous_person = "Winston Churchill";
+var famous_quote = "\"Success is Not Final, Failure is Not Fatal: it is the Courage to Continue that Counts\"";
+var txt_message = "".concat(famous_person, " said, ").concat(famous_quote);
+console.log(txt_message);
+/**
+ * Question 6
+ */
+var personName = "\t \t Abdul \n Hamid \t \t";
+console.log(personName);
+console.log(personName.trim());
+/**
+ * Question 7
+ */
+// Addition
+console.log(5 + 3);
+// Subtraction
+console.log(11 - 3);
+// Multiplication
+console.log(4 * 2);
+// Division
+console.log(16 / 2);
+/**
+ * Question 8
+ */
+console.log(5 + 3);
+console.log(4 + 4);
+console.log(3 + 5);
+console.log(2 + 6);
+/**
+ * Question 9
+ */
+var favrtNum = "7";
+console.log("There are seven ".concat(favrtNum, " heaven and ").concat(favrtNum, " earth"));
+/**
+ * Question 10
+ */
+var myName = "Abdul Hamid";
+console.log("Hello ".concat(fullName, ", would you like to learn some JavaScript Today?"));
+// Quote by Muhammad Ali
+var author_Name = "Muhammad Ali";
+var quoteMessage = "\"He Who is Not Courageous Enough to Take Risks Will Accomplish Nothing in Life\"";
+console.log("".concat(author_Name, " said, ").concat(quoteMessage));
+/**
+ * Question 11
+ */
+var frindNames = ["Shoaib", "Rasool", "Babar", "Zohaib", "Naseem"];
+console.log(frindNames[0], frindNames[1], frindNames[2], frindNames[3], frindNames[4]);
+/**
+ * Question 12
+ */
+var otherFriends = ["Zahoor", "Yawar", "Wasih", "Haseeb", "Zayan"];
+for (var _i = 0, otherFriends_1 = otherFriends; _i < otherFriends_1.length; _i++) {
+    var otherName = otherFriends_1[_i];
+    console.log("".concat(otherName, " is my good friends"));
+}
+/**
+ * Question 13
+ */
+var myTransportCars = ["Motercyle", "Bus"];
+var myTransportMessage = ["go to office", "go to home"];
+for (var i in myTransportCars) {
+    console.log("".concat(myTransportCars[i], " ").concat(myTransportMessage[i]));
+}
+;
+/**
+ * Question 14
+ */
+var invitedFrends = ["Zahoor", "Noman", "Safi", "Hamid", "Rabi"];
+invitedFrends.forEach(function (name) {
+    console.log("Hi ".concat(name, " I would invites you for dinner"));
+});
--- assignment-01/01.ts
+++ assignment-01/01.ts
@@ -1,32 +1,154 @@
-let myName: string = 'Hamid';
-console.log("Hello " + myName);
-
-
-let quote: string = "Learn and apply";
-
-console.log(myName + ": \t" + quote);
-
-console.log(5 + 3);
-console.log(5 - 3);
-console.log(5 * 3);
-console.log(5 / 3);
-
-
-let favoriteNumber: number = 7;
-console.log("This is my favorite number: " + favoriteNumber);
-
-
-// Sigle line comment
 /**
- * Multi line comment
- * Hello this muli line comment
- * Web 3.0 is start
+ * Question 1
+ */ 
+
+/**
+ * Nodejs Typscript and VS Code installed
  */
 
-const names: string[] = ["Saqib", "Hamid", "Sahil", "Amir", "Shahbaz"];
-console.log(names);
 
-console.log("Hello " + names[0]);
-console.log("Hi " + names[1]);
-console.log("Hy" + names[3]);
-console.log("Bye " + names[4]);
+ /**
+  * Question 2
+  */
+
+let fullName: string = "Abdul Hamid"
+console.log(`Hello this is ${fullName}`);
+
+
+/**
+ * Question 3
+ */
+let studentName: string = "Sahil Khan";
+
+// Uppercase
+console.log(studentName.toUpperCase());
+
+// Lowercase
+console.log(studentName.toLowerCase());
+
+// Titlecase
+console.log(`${studentName.charAt(0).toUpperCase()} ${studentName.slice(1)}`);
+
+/**
+ * Question 4
+ */
+
+// Famous Quote by Albert Einstein
+const quote: string = `"Success is Not Final, Failure is Not Fatal: it is the Courage to Continue that Counts"`;
+const authorName: string = `Winston Churchill`;
+
+console.log(`${authorName} said, ${quote}`);
+
+/**
+ * Question 5
+ */
+
+let famous_person: string = `Winston Churchill`;
+let famous_quote: string = `"Success is Not Final, Failure is Not Fatal: it is the Courage to Continue that Counts"`;
+let txt_message: string = `${famous_person} said, ${famous_quote}`;
+
+console.log(txt_message);
+
+
+/**
+ * Question 6
+ */
+
+const personName: string = "\t \t Abdul \n Hamid \t \t";
+console.log(personName);
+console.log(personName.trim());
+
+/**
+ * Question 7
+ */
+
+// Addition
+console.log(5 + 3);
+// Subtraction
+console.log(11 - 3);
+// Multiplication
+console.log(4 * 2);
+// Division
+console.log(16 / 2);
+
+/**
+ * Question 8
+ */
+
+console.log(5 + 3);
+console.log(4 + 4);
+console.log(3 + 5);
+console.log(2 + 6);
+
+/**
+ * Question 9
+ */
+
+let favrtNum: string = `7`;
+console.log(`There are seven ${favrtNum} heaven and ${favrtNum} earth`);
+
+/**
+ * Question 10
+ */
+
+let myName: string = "Abdul Hamid"
+console.log(`Hello ${fullName}, would you like to learn some JavaScript Today?`);
+
+// Quote by Muhammad Ali
+const author_Name: string = `Muhammad Ali`;
+const quoteMessage: string = `"He Who is Not Courageous Enough to Take Risks Will Accomplish Nothing in Life"`;
+
+console.log(`${author_Name} said, ${quoteMessage}`);
+
+/**
+ * Question 11
+ */
+
+let frindNames: string[] = ["Shoaib", "Rasool", "Babar", "Zohaib", "Naseem"];
+console.log(frindNames[0], frindNames[1], frindNames[2], frindNames[3], frindNames[4]);
+
+/**
+ * Question 12
+ */
+
+let otherFriends: string[] = ["Zahoor", "Yawar", "Wasih", "Haseeb", "Zayan"];
+
+for(let otherName of otherFriends){
+    console.log(`${otherName} is my good friends`);
+}
+
+/**
+ * Question 13
+ */
+
+let myTransportCars: string[] = ["Motercyle", "Bus"];
+let myTransportMessage: string[] = ["go to office", "go to home"];
+
+for (let i in myTransportCars) {
+        console.log(`${myTransportCars[i]} ${myTransportMessage[i]}`)
+};
+
+/**
+ * Question 14
+ */
+
+let invitedGuest: string[] = ["Zahoor", "Noman", "Safi", "Hamid", "Rabi"];
+
+invitedGuest.forEach(gName => {
+    console.log(`Hi ${gName} I would like invite you tommarow for dinner`);
+});
+
+
+/**
+ * Question 15 
+ */
+
+let notInvited: string = "Khuram";
+console.log(`Unfortunately, ${notInvited} can't make it to the dinner.\n`);
+
+const newGuest: string = "Bilal";
+gName[gName.indexOf(notInvited)] = newGuest;
+
+// Print updated invitations
+
+console.log(gName);
